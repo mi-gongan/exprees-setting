d06f0d3d04b021ccd36e028ac208b6c8d96093d6
INBUM02 <kib0202@naver.com>
2022-11-02 01:28:29 +0900

    REFACT

---
+++ src/config/index.js
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+function required(key, defaultValue = undefined) {
+  const value = process.env[key] || defaultValue;
+  if (value == null) {
+    throw new Error(`Key ${key} is undefined`);
+  }
+  return value;
+}
+
+export default {
+  host: {
+    cors: required("CORS_ORIGIN", "http://localhost:3000"),
+    port: parseInt(required("HOST_PORT", 8080)),
+  },
+  db: {
+    host: required("DB_HOST"),
+    user: required("DB_USER"),
+    databse: required("DB_DATABASE"),
+    password: required("DB_PASSWORD"),
+    url: required("DB_URL"),
+  },
+};
